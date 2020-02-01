8e7de342283a6a7bb752296b55dfa9d4cbf7568c
angsu0715 <ang_su@naver.com>
2020-02-01 14:16:12 +0900

    init

---
+++ js/swiper.js
@@ -0,0 +1,11 @@
+$(function() {
+	var swiper = new Swiper('.swiper-container', {
+	      slidesPerView: 4,
+	      spaceBetween: 30,
+	      centeredSlides: true,
+	      pagination: {
+	        el: '.swiper-pagination',
+	        clickable: true,
+	      },
+	    });
+})
